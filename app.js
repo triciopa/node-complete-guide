34c98b3dea7c2d0c7cedceea5523b5e1a20e24d4
Patricio <patricio@magnetico.dev>
2021-07-05 20:56:11 -0300

    Router, Views and Utils

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -8,10 +9,10 @@ const shopRoutes = require('./routes/shop');
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const express = require('express');
-
 const router = express.Router();
 
+const rootDir = require('../utils/path');
+
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
-  );
+  // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
