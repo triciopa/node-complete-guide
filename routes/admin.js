9e1e40206f75abf1e55ed4ebf28452f8133ee85d
Patricio <patricio@magnetico.dev>
2021-07-06 11:22:19 -0300

    Export data in the server

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,20 @@ const router = express.Router();
 
 const rootDir = require('../utils/path');
 
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
