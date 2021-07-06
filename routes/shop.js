921852c83659d449dde318879973965aac8b812c
Patricio <patricio@magnetico.dev>
2021-07-06 13:06:23 -0300

    Pug static content

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   // console.log(adminData.products);
   // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
-
-  res.render('shop'); // PUG rendering
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' }); // PUG rendering
 });
 
 module.exports = router;
