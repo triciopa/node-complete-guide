d4a5e6b27f51cc94e4a6d874eda7f487652951b2
Patricio <patricio@magnetico.dev>
2021-07-06 11:56:52 -0300

    Pug rendering

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 // const bodyParser = require('body-parser'); // Deprecated
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,10 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+
+  res.render('shop'); // PUG rendering
 });
 
 module.exports = router;
